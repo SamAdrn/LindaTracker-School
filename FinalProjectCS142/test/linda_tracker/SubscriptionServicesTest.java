b0ef0d15bc87142a11dd107330c9405d4bf435d9
SamAdrn <samadrianko@gmail.com>
2021-10-11 19:03:34 -0400

    Fixed JUnit Tests and misc.

--- FinalProjectCS142/test/linda_tracker/SubscriptionServicesTest.java
+++ FinalProjectCS142/test/linda_tracker/SubscriptionServicesTest.java
@@ -1,20 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package linda_tracker;
 
 import numberlist.BirthDateException;
 import numberlist.IndexException;
 import numberlist.objectlist.BirthDate;
 import numberlist.objectlist.Money;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.io.File;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * This is the JUnit Testing for the SubscriptionServices Class. WARNING:
@@ -29,7 +23,6 @@ public class SubscriptionServicesTest {
 
     SubscriptionServices list;
 
-    @Before
     public void setUp() throws Exception {
         list = new SubscriptionServices();
         deleteList();
